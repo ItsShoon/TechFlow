98c36f07d83c4212a88cf0b85d71ae507e4ab870
rubsribs <a25994@alunos.ipca.pt>
2024-05-27 18:42:53 +0100

    atualiazar

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Navbar from './components/Navbar';
